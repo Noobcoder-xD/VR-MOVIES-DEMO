eeba7bc27c2262dc1e6f2d6dc8b75debe96503db
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-01-20 13:38:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Noobcoder-xD/VR-MOVIES-DEMO /VR-MOVIES-DEMO
+  git clone https://github.com/Noobcoder-xD/VR-MOVIES-DEMO.git /VR-MOVIES-DEMO
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /VR-MOVIES-DEMO 
