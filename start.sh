a2696794a278c8e0d105f95ea323cca9b92baf5d
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-01-20 13:26:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess/tree/Angel_Neha--V8.5.git /LazyPrincess
+  git clone https://github.com/Noobcoder-xD/VR-MOVIES-DEMO /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
