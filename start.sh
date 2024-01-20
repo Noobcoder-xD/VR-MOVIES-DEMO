0c4e1dd03468524a4732627fc69d78318f246ccf
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-01-20 13:36:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Noobcoder-xD/VR-MOVIES-DEMO /LazyPrincess
+  git clone https://github.com/Noobcoder-xD/VR-MOVIES-DEMO /VR-MOVIES-DEMO
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /VR-MOVIES-DEMO 
 fi
-cd /LazyPrincess
+cd /VR-MOVIES-DEMO 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
